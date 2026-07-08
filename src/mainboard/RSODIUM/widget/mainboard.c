9e5006855875b43fe2c41dfe7b3dc15bcfde46d0
barryblueice <barryblueice@outlook.com>
2026-07-08 05:55:42 +0000

    更新主板信息

--- src/mainboard/RSODIUM/widget/mainboard.c
+++ src/mainboard/RSODIUM/widget/mainboard.c
@@ -1,16 +1,39 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/device.h>
+#include <device/pci_ops.h>
+#include <arch/io.h>
 #include <console/console.h>
+#include <option.h>
 #include <southbridge/intel/bd82x6x/pch.h>
 
 static void mainboard_enable(struct device *dev)
 {
-	/*
-	 * Add board-specific initialization here.
-	 * Most things are handled automatically by the Sandy Bridge /
-	 * bd82x6x drivers, so this can be empty for a first boot attempt.
-	 */
+	unsigned int vr_hot;
+	u16 gpio_base;
+	u32 val;
+
+	vr_hot = get_uint_option("vr_hot", 0);
+
+	gpio_base = pci_read_config16(PCH_LPC_DEV, GPIO_BASE) & 0xfffc;
+
+	if (vr_hot) {
+		val = inl(gpio_base + 0x04);
+		val &= ~(1 << 17);
+		outl(val, gpio_base + 0x04);
+
+		val = inl(gpio_base + 0x0c);
+		val &= ~(1 << 17);
+		outl(val, gpio_base + 0x0c);
+
+		printk(BIOS_INFO, "VR_HOT#: forced low\n");
+	} else {
+		val = inl(gpio_base + 0x04);
+		val |= (1 << 17);
+		outl(val, gpio_base + 0x04);
+
+		printk(BIOS_INFO, "VR_HOT#: disabled (input)\n");
+	}
 }
 
 struct chip_operations mainboard_ops = {
